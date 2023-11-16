4fa29194e1a2c0c8d4bafa3a2b951319b70b040b
kennedygamarra <kennedy.gamarra@hypersoft.co>
2023-11-16 10:47:55 -0500

    fix: column names unnecessary

--- microservice-student/src/main/java/com/microservice/student/entities/Student.java
+++ microservice-student/src/main/java/com/microservice/student/entities/Student.java
@@ -16,11 +16,9 @@ public class Student {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @Column(name = "name")
     private String name;
     @Column(name = "last_name")
     private String lastName;
-    @Column(name = "email")
     private String email;
     @Column(name = "course_id")
     private Long courseId;
